093d3bc4a6f29d49bee88274151bd7f0fa89c786
Muhammad Ahmad Fridi <muhammadahmadfridi71al@gmail.com>
2025-06-15 19:28:04 +0500

    Update Body2.jsx

--- src/components/Body2.jsx
+++ src/components/Body2.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import img222 from "../images/img88.jpg";
 
-
 const About = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [selectedSkill, setSelectedSkill] = useState(null);
@@ -13,7 +12,6 @@ const About = () => {
     { name: "Cleaning App", url: "https://wix-cleanic-project.vercel.app/" },
     { name: "Api Product", url: "https://perfect-product-api.vercel.app/" },
     { name: "Mejiwoo Fashion", url: "https://mejiwoo-fashion-project.vercel.app/" },
-
   ]);
 
   const [creativeProjects] = useState([
@@ -182,24 +180,23 @@ const About = () => {
         )}
       </section>
 
-<div className="min-h-[400px] bg-[#1b1a1b] flex flex-col items-center p-5 md:p-10 text-white">
-  <img
-    src={img222}
-    alt="User Icon"
-    className="w-60 h-60 md:w-80 md:h-80 rounded-full object-contain object-center bg-white p-2 shadow-lg border-4 border-white"
-  />
-  <h1 className="text-3xl md:text-4xl font-bold mt-5 mb-3">
-    Muhammad Ahmad Fridi
-  </h1>
-  <p className="text-center text-gray-300 max-w-2xl mb-10">
-    I'm a passionate developer with a strong ambition to become a
-    full-stack developer. I’m currently sharpening my skills across both
-    front-end and back-end technologies. My future plan is to build my own
-    software house and contribute to impactful digital solutions.
-  </p>
-</div>
-
-
+      <div className="min-h-[400px] bg-[#1b1a1b] flex flex-col items-center p-5 md:p-10 text-white">
+        <img
+          src={img222}
+          alt="User Icon"
+          className="w-60 h-60 md:w-80 md:h-80 rounded-full object-contain object-center bg-white p-2 shadow-lg border-4 border-white"
+        />
+        <h1 className="text-3xl md:text-4xl font-bold mt-5 mb-3">
+          Muhammad Ahmad Fridi
+        </h1>
+        <p className="text-center text-gray-300 max-w-2xl mb-10">
+          I'm a passionate developer with a strong ambition to become a
+          full-stack developer. I’m currently sharpening my skills across both
+          front-end and back-end technologies. My future plan is to build my own
+          software house and contribute to impactful digital solutions.
+        </p>
+      </div>
+    </>
   );
 };
 
